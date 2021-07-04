ae92e4f3b973f55f49451d642c1519df057af415
cestbonciel <46340978+cestbonciel@users.noreply.github.com>
2021-07-05 03:31:02 +0900

    Edited Drum kit to use JAVASCRIPT by cestbonciel
    I edited Drum kit. and I tried to apply responsive web.

---
+++ 01cestbonciel_JavaScript Drum Kit/sound.js
@@ -0,0 +1,27 @@
+function playSound(e){
+    
+    // console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // console.log(audio);
+    if(!audio) return ;//stop the function from running all together
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    // console.log(key);
+    key.classList.add('playing');
+  
+  };
+  
+  const keys = document.querySelectorAll('.key');
+  
+  function removeTransition(e){
+    // console.log(e);
+    if(e.propertyName !== 'transform') return; //skip it if it's not a transform.
+    // console.log(e.propertyName);
+    // console.log(this);
+    this.classList.remove('playing');
+  };
+  
+  keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+  
+  window.addEventListener('keydown',playSound);
